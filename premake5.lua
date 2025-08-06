25d3060e4635a602b88214bdb275b36b2f2babbb
olenhardt <o.lenhardt@web.de>
2025-08-06 15:11:23 +0200

    prmakeUpdate

--- premake5.lua
+++ premake5.lua
@@ -1,25 +1,41 @@
-project "Glad"
+project "ImGui"
 	kind "StaticLib"
-	language "C"
+	language "C++"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-        "include/glad/glad.h",
-        "include/KHR/khrplatform.h",
-        "src/glad.c"
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imgui_rectpack.h",
+		"imgui_textedit.h",
+		"imgui_truetype.h",
+		"imgui_demo.cpp"
 	}
-
-    includedirs
-    {
-        "include"
-    }
 	
 	filter "system:windows"
-        systemversion "latest"
-        staticruntime "On"
+		systemversion "latest"
+		cppdialect "C++20"
+		staticruntime "On"
 
-    filter {"system:windows", "configurations:Release"}
-        buildoptions "/MT"
+
+		defines 
+		{ 
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+		postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox/")
+        }
+
+
+	filter {"system:windows", "configurations:Release"}
+			buildoptions "/MT"
